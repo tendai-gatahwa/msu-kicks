807a366640ad67206de07c349f851548076c526e
tendai-gatahwa <107326659+tendai-gatahwa@users.noreply.github.com>
2023-09-04 19:27:14 +0200

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("edc035e6-c524-489c-992f-7cfc2b1fdc19");
 
   return (
     <Container>
